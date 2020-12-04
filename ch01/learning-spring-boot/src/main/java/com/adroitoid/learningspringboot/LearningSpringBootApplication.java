1a5780e1fefdd594a3bf147e300d03eaf26b9c9e
Paul Lewallen <paul.lewallen@gmail.com>
2020-12-04 11:11:04 -0800

    Built basic api. localhost:8080 and localhost:8080?name=Paul MongoDB is installed as service.

---
+++ ch01/learning-spring-boot/src/main/java/com/adroitoid/learningspringboot/LearningSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.adroitoid.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(LearningSpringBootApplication.class, args);
+	}
+
+}
---
+++ ch01/learning-spring-boot/src/test/java/com/adroitoid/learningspringboot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adroitoid.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
